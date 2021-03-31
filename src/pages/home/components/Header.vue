1d86462c505d2f8a0ae59814cb388062b9bc5294
ynaonao <1606939624@qq.com>
2021-03-31 14:32:20 +0800

    add header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont back-icon">&#xe624;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe617;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style scoped lang="scss">
+  @import '~styles/variables.scss';
+  .header{
+    line-height: .86rem;
+    display: flex;
+    background: $bgColor;
+    color: #fff;
+    .header-left{
+      width: .63rem;
+      float:left;
+      .back-icon{
+        display: block;
+        text-align: center;
+        font-size: .4rem;
+      }
+    }
+    .header-input{
+      flex: 1;
+      height: .64rem;
+      line-height: .64rem;
+      background: #fff;
+      margin-top: .12rem;
+      margin-left: .2rem;
+      border-radius: .1rem;
+      padding-left: .2rem;
+      color:#ccc;
+    }
+    .header-right{
+      width: 1.24rem;
+      float: right;
+      text-align: center;
+    }
+  }
+</style>
